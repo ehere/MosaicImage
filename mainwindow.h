09b83924b5e2f1db96cdb8c67ef675066002cb2e
Tanasab Permpool <tanasab2@gmail.com>
2016-11-16 21:50:38 +0700

    Can mosaic when click

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,27 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QPicture>
+#include <iostream>
+#include <opencv2\opencv.hpp>
+#include <windows.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <boost/filesystem.hpp>
+#include <iomanip>
+#include <direct.h>
+#include <ctime>
+#include "exif.h"
+
+namespace fs = ::boost::filesystem;
+
+using namespace std;
+using namespace cv;
+using namespace std;
 
 namespace Ui {
 class MainWindow;
@@ -14,7 +35,18 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-
+    string targetFolder;
+    string originalImageFilePath;
+    string imagesSetFolder;
+    string pixelFolder;
+    string index_filename = "index.txt";
+    Size pixelSize;
+    string input_image;
+    double resize_x;
+    double resize_y;
+    char pixelize;
+    char mosaicize;
+    unsigned skip_interval;
 private slots:
     void on_btn_1_clicked();
 
